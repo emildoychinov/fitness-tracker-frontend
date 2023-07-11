b390e1845130730199fdef1517bda3222367da31
emil <bgemo105@gmail.com>
2023-07-11 15:10:15 +0300

    login + register

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { RouteComponentProps } from "react-router-dom";
 import { RouterPathEnum } from "src/enums/RouterPathEnum";
+import { postToRoute } from "src/requests";
 
 
 class Login extends React.Component<RouteComponentProps<Login>, {}> {
